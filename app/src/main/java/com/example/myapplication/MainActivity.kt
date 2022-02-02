601d0baf29c0c56ad500f524a334124fc69be7aa
JH <harry7408@naver.com>
2022-02-02 17:48:56 +0900

    first commit(시작화면, 언어선택화면)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button1 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1=findViewById<Button>(R.id.main_button1)
+        button1.setOnClickListener {
+            val intent= Intent(this@MainActivity,ChooseLang::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
